4202c6972db6db1bc2bc79bc5588358d14487455
m-nikhil <m_nikhil@outlook.com>
2017-06-08 22:25:20 +0530

    base structure

---
+++ brace-change/brace-change.sh
@@ -0,0 +1,4 @@
+cp -f brace-change/mode-chapel.js node_modules/brace/mode/chapel.js
+cp -f brace-change/modelist.js node_modules/brace/ext/modelist.js
+cp -f brace-change/snippets-chapel.js node_modules/brace/snippets/chapel.js
+
---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+
+var debug = process.env.NODE_ENV !== "production";
+var webpack = require('webpack');
+var path = require('path');
+
+module.exports = {
+  context: path.join(__dirname, "src"),
+  devtool: debug ? "inline-sourcemap" : null,
+  entry: "./js/client.js",
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['react', 'es2015', 'stage-0'],
+          plugins: ['react-html-attrs', 'transform-class-properties', 'transform-decorators-legacy'],
+        }
+      },
+      {
+        test: /(\.css)$/,
+        loader: 'style-loader!css-loader'
+      },
+      {
+        test: /\.jpe?g$|\.gif$|\.png$|\.ttf$|\.eot$|\.svg$/,
+        use: 'file-loader?name=[name].[ext]?[hash]'
+      },
+      {
+        test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: 'url-loader?limit=10000&mimetype=application/fontwoff'
+      }
+    ]
+  },
+  output: {
+    path: __dirname + "/src/",
+    filename: "client.min.js"
+  },
+  plugins: debug ? [] : [
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.optimize.UglifyJsPlugin({ mangle: false, sourcemap: false }),
+  ],
+};
