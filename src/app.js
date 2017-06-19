e79657aa69a7de1991fc73b18c489f8144280ed7
m-nikhil <m_nikhil@outlook.com>
2017-06-20 01:59:19 +0530

    socket.io server

--- src/app.js
+++ src/app.js
@@ -1,19 +1,48 @@
 const express = require('express');
-const path = require('path');
 const app = express();
 
 if(process.env.NODE_ENV == "production") {
-
-app.use(express.static(path.join(__dirname, '../../build')));
-
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, '../../build', 'index.html'));
-});
-
+  const servestatic = require('./controller/staticfilesController');
+  app.use('/',servestatic);
 }
 
-app.get('/api', function (req, res) {
-  res.send('Hello Chapel');
+
+
+/*
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+*/
+
+
+
+
+
+app.use(function(req, res, next) {
+        res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+        res.header("Access-Control-Allow-Headers", "X-Requested-With");
+        res.header("Access-Control-Allow-Headers", "Content-Type");
+        res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
+        res.header("Access-Control-Allow-Credentials", "true");
+        next();
+    });
+
+
+app.get('*/*', function (req, res) {
+  res.send("hey!");
+});
+const server = require('http').Server(app);
+let io = require('socket.io').listen(server);
+
+//io.set('origins', 'http://localhost:3002');
+
+io.on ("connection", function(socket){
+           socket.emit('news', { hello: 'world' });
+        socket.on("run", function (data){
+		    console.log("yeah! socket works");
+	});
 });
 
 module.exports = app;
---
+++ src/controller/staticfilesController.js
@@ -0,0 +1,12 @@
+var express = require('express');
+const path = require('path');
+var router = express.Router();
+
+
+router.use(express.static(path.join(__dirname, '../../../chapel-online/build')));
+
+  router.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, '../../../chapel-online/build', 'index.html'));
+});
+
+module.exports = router;
