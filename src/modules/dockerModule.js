84228617b4c4d15caedcdc5a681bdec578efe281
m-nikhil <m_nikhil@outlook.com>
2017-06-23 01:24:04 +0530

    remove compiling text

--- src/modules/dockerModule.js
+++ src/modules/dockerModule.js
@@ -6,11 +6,10 @@ function docker (socket, code, stdin ) {  // add flags
 
   let script = ' echo $0 > service.chpl;\
 		             echo $1 >input.txt;\
-                 echo "Compiling..."; \
                  chpl -o service service.chpl;\
 		             ./service < input.txt;';
 
- //needs to be replaced with a UUID peralink
+ //needs to be replaced with a UUID permalink
  var containerID = (Math.random() * 7983 +1).toString();
 
   let OPTIONS = {
