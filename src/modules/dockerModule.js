c4eaa1f6031595092710d02a95dad66e74a4ae70
m-nikhil <m_nikhil@outlook.com>
2017-06-23 01:38:11 +0530

    added chapel-gasnet docker image

--- src/modules/dockerModule.js
+++ src/modules/dockerModule.js
@@ -7,14 +7,14 @@ function docker (socket, code, stdin ) {  // add flags
   let script = ' echo $0 > service.chpl;\
 		             echo $1 >input.txt;\
                  chpl -o service service.chpl;\
-		             ./service < input.txt;';
+		             ./service -nl 2 < input.txt;'       // nl must take value from a drop list, provided in the UI (future)
 
  //needs to be replaced with a UUID permalink
  var containerID = (Math.random() * 7983 +1).toString();
 
   let OPTIONS = {
     'name': containerID,
-    'Image': 'chapel/chapel',
+    'Image': 'chapel/chapel-gasnet',
     'AttachStdin': true,
     'AttachStdout': true,
     'AttachStderr': true,
